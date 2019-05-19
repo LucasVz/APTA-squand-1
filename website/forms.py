98a0443502b6526498f7205cd3624c0646f2ac4b
mistahmidnighto <49170441+mistahmidnighto@users.noreply.github.com>
2019-05-18 22:15:08 -0300

    Add files via upload

--- website/forms.py
+++ website/forms.py
@@ -3,13 +3,6 @@ from django import forms
 
 class ContactHelp(forms.Form):
     nome = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Seu nome', 'data-rule':'minlen:4', 'data-msg': 'Por favor, insira pelo menos 3 caracteres.'}))
-    telefone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '(xx)xxxx-xxxxx', 'data-rule':'minlen:4', 'data-msg': 'Por favor, insira pelo menos 9 caracteres.'}), max_length=12)
+    telefone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '(xx)xxxxxxxxx', 'data-rule':'minlen:4', 'data-msg': 'Por favor, insira pelo menos 9 caracteres.'}), max_length=11)
     email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'exemplo@exemplo.com', 'data-rule':'minlen:4'}), label='E-mail')
     mensagem = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Duvida/Mensagem', 'data-rule':'minlen:4'}))
-
-    #class Meta:
-    #    model = Post
-    #    fields = ('title', 'text',)
-
-
-
--- website/views.py
+++ website/views.py
@@ -5,14 +5,6 @@ from .models import QuemSomos, Portfolio, Clientes, Servicos
 
 def website(request):
 
-    if request.method == 'POST':
-        form = ContactHelp(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            telefone = form.cleaned_data['telefone']
-            message = form.cleaned_data['message']
-
     template_name = 'index.html'
     context_dict = {}
     context_dict['data'] = QuemSomos.objects.last()
@@ -20,4 +12,14 @@ def website(request):
     context_dict['stuff'] = Portfolio.objects.all()
     context_dict['opcoes'] = Servicos.objects.all()
     context_dict['form'] = ContactHelp()
+
+    if request.method == 'POST':
+        form = ContactHelp(request.POST)
+        if form.is_valid():
+            context_dict['is_valid'] = True
+            #form.send_mail()
+            form = ContactHelp()
+    else:
+        form = ContactHelp()
+
     return render(request, template_name, context_dict)
